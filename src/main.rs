21f516b2d099ac11743627182327694db13d4c94
carter <speers.carter@gmail.com>
2024-03-29 00:54:01 -0400

    added more coins and easier to add more

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,49 @@ use reqwest::Client;
 use tinytemplate::TinyTemplate;
 use actix_rust_template::get_coin_price;
 
+
 #[derive(serde::Deserialize)]
 struct QueryParams {
     id: String,
 }
 
+
 #[get("/coin_values")]
-async fn coin_values(params: web::Query<QueryParams>) -> impl Responder {// Create a reqwest Client
+async fn coin_values() -> impl Responder {// Create a reqwest Client
+
+    const COINS: [&str; 5] =
+        [
+            "bitcoin",
+            "ethereum",
+            "solana",
+            "cardano",
+            "tether",
+
+
+        ];
     let html_content = fs::read_to_string("app/index.html").unwrap();
     let mut template = TinyTemplate::new();
     template.add_template("coin_values_templ", &html_content).unwrap();
 
-    let mut context: HashMap<String, f64> = HashMap::from(
-        [
-            ("bitcoin_price".to_string(), 0f64),
-            ("ethereum_price".to_string(), 0f64),
-            ("binance_coin_price".to_string(), 0f64)
-        ]
-    );
+    let mut context: HashMap<String, String> = HashMap::new();
 
-    if let Ok(bitcoin_price) = get_coin_price(&params.id).await {
-        context.insert(format!("{}_price", &params.id), bitcoin_price[&params.id]["usd"]);
-
-        let rendered = template.render("coin_values_templ", &context).unwrap();
-        HttpResponse::Ok()
-            .content_type("text/html")
-            .body(rendered)
-    } else {
-
-
-        // Return an error if the request was not successful
-        HttpResponse::InternalServerError().body("womp womp")
+    for coin in COINS {
+        if let Ok(bitcoin_price) = get_coin_price(coin).await {
+            context.insert(
+                coin.to_string(),
+                format!("{} : ${}", coin, bitcoin_price[coin]["usd"]),
+            );
+        }
     }
+    println!("{context:?}");
+    println!("{}", &html_content);
+
+    let rendered = template.render("coin_values_templ", &context).unwrap();
+
+    println!("{}", &rendered);
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(rendered)
 }
 
 
